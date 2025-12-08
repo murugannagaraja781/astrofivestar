7d56d42a43ebe5fa2b86c60caaa2f8372d8cea25
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-12-08 10:01:52 +0530

    swork

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Polyfills are handled by vite-plugin-node-polyfills in vite.config.js
-// Do not manually override Buffer with empty array as it breaks simple-peer
+// Polyfill for process/buffer in browser for simple-peer
+import * as process from "process";
+window.global = window;
+window.process = process;
+window.Buffer = [];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
